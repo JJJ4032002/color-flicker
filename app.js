c92833bc20c7da57d416ce832f5cafa929298d1b
JJJ4032002 <rp4888888@gmail.com>
2021-05-09 10:57:26 +0530

    index commit

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const simple = document.querySelector("p1");
 const hex = document.querySelector("#p2");
 const btn = document.querySelector("#button");
+const color = document.querySelector("#color");
 
+ let colors = ["Red","Blue","Green","Orange","Purple","Gray","Maroon"];
+
+  btn.addEventListener("click",() => {
+      let i = Math.floor(Math.random()*7);
+
+      document.body.style.backgroundColor = colors[i];
+      color.innerHTML = colors[i];
+  });
